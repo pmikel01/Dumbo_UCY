7eeb3ffdc427c1907939e1ec8b137455b0a76e0b
Yuan Lu <yl768@njit.edu>
2020-12-29 16:43:25 +0800

    change erasure code

--- dumbobft/core/provablereliablebroadcast.py
+++ dumbobft/core/provablereliablebroadcast.py
@@ -2,43 +2,44 @@
 import time
 from collections import defaultdict
 from typing import List
-
 import gevent
 from gevent import monkey
 from crypto.threshsig.boldyreva import serialize, deserialize1
 from honeybadgerbft.core.reliablebroadcast import merkleTree, getMerkleBranch, merkleVerify
-from pyeclib.ec_iface import ECDriver
+from honeybadgerbft.core.reliablebroadcast import encode, decode
+#from pyeclib.ec_iface import ECDriver
+
 monkey.patch_all()
 
-
-def encode(K: int, N: int, m):
-    coder = ECDriver(k=K, m=N-K, ec_type='isa_l_rs_vand')
-    assert coder.k == K
-    assert coder.m == N - K
-    try:
-        m = m.encode()
-    except AttributeError:
-        pass
-    stripes = [_ for _ in coder.encode(m)]
-    #assert len(stripes[0]) == len(stripes[-1])
-    #print(str(len(stripes[0])))
-    return stripes
-
-def decode(K: int, N: int, stripes: List[bytes]):
-    coder = ECDriver(k=K, m=N-K, ec_type='isa_l_rs_vand')
-    #assert len(stripes) == N
-    #assert len(stripes) == coder.k + coder.m
-    blocks = []
-    for block in stripes:
-        if block is None:
-            continue
-        blocks.append(block)
-        if len(blocks) == K:
-            break
-    else:
-        raise ValueError("Too few to recover")
-    rec = coder.decode(blocks)
-    return rec
+#
+# def encode(K: int, N: int, m):
+#     coder = ECDriver(k=K, m=N-K, ec_type='isa_l_rs_vand')
+#     assert coder.k == K
+#     assert coder.m == N - K
+#     try:
+#         m = m.encode()
+#     except AttributeError:
+#         pass
+#     stripes = [_ for _ in coder.encode(m)]
+#     #assert len(stripes[0]) == len(stripes[-1])
+#     #print(str(len(stripes[0])))
+#     return stripes
+#
+# def decode(K: int, N: int, stripes: List[bytes]):
+#     coder = ECDriver(k=K, m=N-K, ec_type='isa_l_rs_vand')
+#     #assert len(stripes) == N
+#     #assert len(stripes) == coder.k + coder.m
+#     blocks = []
+#     for block in stripes:
+#         if block is None:
+#             continue
+#         blocks.append(block)
+#         if len(blocks) == K:
+#             break
+#     else:
+#         raise ValueError("Too few to recover")
+#     rec = coder.decode(blocks)
+#     return rec
 
 
 
