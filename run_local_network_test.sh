3cb2f73ee60b6a16ac23902ee20f07fd5b0e1b34
pmikel01 <pmikel01@ucy.ac.cy>
2022-02-13 20:39:07 +0200

    Fix sdumbo

--- run_local_network_test.sh
+++ run_local_network_test.sh
@@ -11,7 +11,7 @@ i=0
 while [ "$i" -lt $1 ]; do
     echo "start node $i..."
     # python3 run_socket_node.py --sid 'sidA' --id $i --N $1 --f $2 --B $3 --K $4 --S 2000 --T 1 --F 1000000 --P "hotstuff1" &
-    python3 run_socket_node.py --sid 'sidA' --id $i --N $1 --f $2 --B $3 --K $4 --T 100 --P "dumbo" --F 1000000 > nodelogs/node-$i.log &
+    python3 run_socket_node.py --sid 'sidA' --id $i --N $1 --f $2 --B $3 --K $4 --T 100 --P "sdumbo" --F 1000000 > nodelogs/node-$i.log &
     i=$(( i + 1 ))
 done
 wait
