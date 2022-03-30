abe739a3ef2fde8e7f830da0790ebd2ab04f6197
pmikel01 <pmikel01@ucy.ac.cy>
2022-03-30 23:42:40 +0300

    Minor

--- run_socket_node.py
+++ run_socket_node.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
                 priv_ip = params[1]
                 pub_ip = params[2]
                 # port = int(params[3])
-                port = 10200
+                port = 10400
                 # print(pid, ip, port)
                 if pid not in range(N):
                     continue
