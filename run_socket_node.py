7214cd095cdfd6bb91b9eefa74ec2110bfa2f145
pmikel01 <pmikel01@ucy.ac.cy>
2022-04-01 22:46:45 +0300

    f

--- run_socket_node.py
+++ run_socket_node.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
                 priv_ip = params[1]
                 pub_ip = params[2]
                 # port = int(params[3])
-                port = 11400
+                port = 11600
                 # print(pid, ip, port)
                 if pid not in range(N):
                     continue
